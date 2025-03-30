89cd71e072fbbf5ab1752544d18bf017b28bd5e0
Cristhian Castillo <kal2403199255@gmail.com>
2025-03-30 20:55:28 +0200

    Add Hero component with responsive design

---
+++ src/components/Hero.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import heroImage from "../assets/hero-image.png";
+
+const Hero = () => {
+  return (
+    <section className='container mx-auto flex flex-col md:flex-row justify-between items-center pt-44 pb-6 px-4 sm:px-6 lg:px-8'>
+        
+
+
+    </section>
+  )
+}
+
+export default Hero;
