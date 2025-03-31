477f4a4404d10c347efa26e5fd604aa2bf98e52a
Cristhian Castillo <kal2403199255@gmail.com>
2025-03-31 22:20:16 +0200

    Added structure of company logos and styles

--- src/components/CompanyLogo.jsx
+++ src/components/CompanyLogo.jsx
@@ -11,6 +11,18 @@ const CompanyLogo = () => {
 
   return (
     <div className='w-full overflow-hidden container mx-auto py-20 gap-8 flex sm:flex-row flex-col sm:items-center items-start'>
+        <div className='w-[300px] shrink-0 px-5 text-gray-600 border-l-4 border-blue-500 bg-white py-2 z-10 sm:text-base text-xl font-semibold text-left'>Proud partner at <br/> Hubspot & Segment</div>
+
+        <div className='flex animate-marquee whitespace-nowrap'>
+            {logos.map((logo, index) => (
+                <img key={index} src={logo}  alt='company logo' className='mx-12 h-8 w-26 object-contain grayscale opacity-70 hover:grayscale-0 hover:opacity-100 transition-all'/>
+            ))}
+
+
+            {logos.map((logo, index) => (
+                <img key={`duplicate-${index}`} src={logo}  alt='company logo' className='mx-12 h-8 w-26 object-contain grayscale opacity-70 hover:grayscale-0 hover:opacity-100 transition-all'/>
+            ))}
+        </div>
     </div>
   )
 }
