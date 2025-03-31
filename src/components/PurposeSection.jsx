61744e6a95b456236660c4a97e6a254cabd99780
Cristhian Castillo <kal2403199255@gmail.com>
2025-03-31 22:27:51 +0200

    Update PurposeSection with structure and styles tailwindcss

--- src/components/PurposeSection.jsx
+++ src/components/PurposeSection.jsx
@@ -18,6 +18,36 @@ const PurposeSection = () => {
   return (
     <section className='w-full bg-gray-50 py-16 px-4 sm:px-6 md:px-8'>
 
+        <div className='max-w-6xl mx-auto'>
+
+            <div className='grid md:grid-cols-3 grid-cols-1 gap-8'>
+
+                <div className=''>
+                    <p className='text-sm text-purple-600 font-medium mb-2'>ACHIEVE MORE</p>
+                    <h2 className='text-3xl md:w-4/5 w-full md:text-4xl font-bold text-gray-900'>Purpose of a convoy is to keep your team</h2>  
+                </div>
+                
+                <div className='col-span-2 grid grid-cols-1 md:grid-cols-2 justify-between gap-8'>
+                    {
+                        features.map((feature, index) => (
+                            <div key={index} className='flex space-x-4'>
+
+                                <div className='w-12 h-12 flex items-center justify-start rounded-lg'>{feature.icon}</div>
+
+                                <div>
+                                    <h3 className='text-xl font-semibold text-gray-900 mb-2'>{feature.title}</h3>
+                                    <p className='text-gray-600'>{feature.description}</p>
+                                </div>
+
+                            </div>
+                        ))
+                    }
+                </div>
+
+            </div>
+        
+        </div>
+
     </section>
   )
 }
