b2366a9b407cb128c400b1cc8e138967bcc44b9c
Cristhian Castillo <kal2403199255@gmail.com>
2025-04-01 22:37:16 +0200

    added ID

--- src/components/PurposeSection.jsx
+++ src/components/PurposeSection.jsx
@@ -16,7 +16,7 @@ const PurposeSection = () => {
       ];
 
   return (
-    <section className='w-full bg-gray-50 py-16 px-4 sm:px-6 md:px-8'>
+    <section id='about' className='w-full bg-gray-50 py-16 px-4 sm:px-6 md:px-8'>
 
         <div className='max-w-6xl mx-auto'>
 
