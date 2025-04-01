352ea79f340bbe0fdb7411ca0e2cbdbde9bf0f5a
Cristhian Castillo <kal2403199255@gmail.com>
2025-04-01 22:30:33 +0200

    Added new component featuresSection

---
+++ src/components/FeaturesSection.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const FeaturesSection = () => {
+
+  return (
+    <section className='max-w-7xl mx-auto px-4 py-16 '>
+
+    </section>
+  )
+}
+
+export default FeaturesSection;
