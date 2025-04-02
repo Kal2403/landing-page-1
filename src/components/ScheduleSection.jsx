b6964c8699af9ed657231910d0053116080b73a6
Cristhian Castillo <kal2403199255@gmail.com>
2025-04-02 23:51:58 +0200

    Adde img and styles tailwind of section

--- src/components/ScheduleSection.jsx
+++ src/components/ScheduleSection.jsx
@@ -5,6 +5,15 @@ import { IoIosArrowRoundForward } from "react-icons/io";
 const ScheduleSection = () => {
   return (
     <section className='max-w-7xl mx-auto px-4 py-16 md:py-24'>
+
+        <div className='flex flex-col md:flex-row items-center justify-between gap-12 md:gap-24'>
+
+            <div className='md:w-1/2 w-full'>
+                <img src={scheduleImage} alt="schedule image" className='w-full h-auto' />
+            </div>
+
+        </div>
+        
     </section>
   )
 }
