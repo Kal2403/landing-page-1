7c39a0218ec2939c73d51f7dbbe3c5d92c7be3f3
Cristhian Castillo <kal2403199255@gmail.com>
2025-04-02 23:51:06 +0200

    Added schedule component

---
+++ src/components/ScheduleSection.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import scheduleImage from "../assets/stats.webp";
+import { IoIosArrowRoundForward } from "react-icons/io";
+
+const ScheduleSection = () => {
+  return (
+    <section className='max-w-7xl mx-auto px-4 py-16 md:py-24'>
+    </section>
+  )
+}
+
+export default ScheduleSection;
