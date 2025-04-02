8b8c39abebf0870e62857fdf189d0f15abbacaec
Cristhian Castillo <kal2403199255@gmail.com>
2025-04-02 23:53:16 +0200

    Update schedule with description and tailwindcss styles

--- src/components/ScheduleSection.jsx
+++ src/components/ScheduleSection.jsx
@@ -12,6 +12,16 @@ const ScheduleSection = () => {
                 <img src={scheduleImage} alt="schedule image" className='w-full h-auto' />
             </div>
 
+            <div className='md:w-1/2 w-full'>
+                <p className='text-orange-500 font-semibold'>SCHEDULE</p>
+                <h2 className='text-3xl md:text-4xl font-bold text-neutral-900 mt-4 mb-6'>Streamline Your Business <br /> With Smart Scheduling Solutions</h2>
+                <p className='text-gray-600 mb-8'>Take control of your time and boost productivity with our intelligent schedule system. Automate appointments, manage team availability, and deliver exceptional customer experiences through seamless calendar management.</p>
+                <a href="#" className='text-blue-500 font-semibold flex items-center gap-2 hover:gap-4 transition-all'>
+                Explore Scheduling
+                <IoIosArrowRoundForward className='size-8' />
+                </a>
+            </div>
+
         </div>
         
     </section>
