d05f4a4909ddc2c304487f1ae99db81d95706d7c
Cristhian Castillo <kal2403199255@gmail.com>
2025-04-03 22:37:35 +0200

    Added pricing calculation logic

--- src/components/PricingSection.jsx
+++ src/components/PricingSection.jsx
@@ -4,6 +4,9 @@ const PricingSection = () => {
     
     const [ productCount, setProductCount ] = useState(0);
 
+    const starterPrice = Math.round(4000 * (productCount / 50));
+    const businessPrice = Math.round(7500 * (productCount / 50));
+
   return (
     <section className='py-20 px-4'>
 
