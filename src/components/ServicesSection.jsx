5893a709c0866a99d85318cce498ca761b8c7558
Cristhian Castillo <kal2403199255@gmail.com>
2025-04-04 23:45:59 +0200

    Added services data with icons for cards

--- src/components/ServicesSection.jsx
+++ src/components/ServicesSection.jsx
@@ -1,6 +1,37 @@
 import React from 'react';
+import { BiTime } from 'react-icons/bi';
+import { BsStack } from 'react-icons/bs';
+import { FiSettings } from 'react-icons/fi';
+import { HiLightBulb } from 'react-icons/hi';
 
 const ServicesSection = () => {
+
+    const services = [
+        {
+            icon: <BsStack className="w-8 h-8 text-indigo-600" />,
+            title: "Web Design",
+            description: "One for all and all for one, Muskehounds are always ready.",
+            link: "#learn-more"
+        },
+        {
+            icon: <HiLightBulb className="w-8 h-8 text-amber-400" />,
+            title: "Ad-Creatives",
+            description: "Alphabet Village and the subline of her own road.",
+            link: "#learn-more"
+        },
+        {
+            icon: <FiSettings className="w-8 h-8 text-red-400" />,
+            title: "Automation",
+            description: "Little Blind Text should turn around and return.",
+            link: "#learn-more"
+        },
+        {
+            icon: <BiTime className="w-8 h-8 text-cyan-400" />,
+            title: "Infographics",
+            description: "Nothing the copy said could convince her.",
+            link: "#learn-more"
+        }
+    ]
 
     return (
         <section className='py-20 container mx-auto px-4 sm:px-6 lg:px-8'>
