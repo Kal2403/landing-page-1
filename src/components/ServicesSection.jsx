48d0f29e96291cea52411fc0d292d9514cf2a19b
Cristhian Castillo <kal2403199255@gmail.com>
2025-04-04 23:42:55 +0200

    Added new component ServicesSection

---
+++ src/components/ServicesSection.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const ServicesSection = () => {
+
+    return (
+        <section className='py-20 container mx-auto px-4 sm:px-6 lg:px-8'>
+
+            <div className='flex flex-col md:flex-row items-center justify-between gap-12 lg:gap-24'>
+
+            </div>
+
+        </section>
+    )
+}
+
+export default ServicesSection;
