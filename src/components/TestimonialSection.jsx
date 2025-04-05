1b327a293d525c836c30a0a14f52fcb0b22568d3
Cristhian Castillo <kal2403199255@gmail.com>
2025-04-05 22:21:19 +0200

    Create initial conponent structure Testimonial

---
+++ src/components/TestimonialSection.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const TestimonialSection = () => {
+    return (
+        <section className='py-16 px-4 max-w-7xl mx-auto'>
+
+        </section>
+    )
+}
+
+export default TestimonialSection;
