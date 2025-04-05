fec520b726320f718c01a1f149bbd5b1b5ccbcf1
Cristhian Castillo <kal2403199255@gmail.com>
2025-04-06 00:17:38 +0200

    Create inital component sctructure Newsletter

---
+++ src/components/NewsletterSection.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NewsletterSection = () => {
+  return (
+    <section className='container mx-auto px-4 sm:px-6 lg:px-8 py-16'>
+
+    </section>
+  )
+}
+
+export default NewsletterSection;
