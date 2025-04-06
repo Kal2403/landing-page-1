ea11c8c10eb0871d85b3eac7e7f91876882ca455
Cristhian Castillo <kal2403199255@gmail.com>
2025-04-06 22:32:25 +0200

    Add layout and background slant

--- src/components/NewsletterSection.jsx
+++ src/components/NewsletterSection.jsx
@@ -1,11 +1,26 @@
 import React from 'react';
 
 const NewsletterSection = () => {
-  return (
-    <section className='container mx-auto px-4 sm:px-6 lg:px-8 py-16'>
+    return (
+        <section className='container mx-auto px-4 sm:px-6 lg:px-8 py-16'>
 
-    </section>
-  )
+            <div className='bg-blue-600 rounded-2xl overflow-hidden'>
+
+                <div className='relative md:px-16 py-16 px-6 md:py-24'>
+                    <div className='absolute top-0 right-0 w-1/2 h-full bg-blue-700 clip-path-slant hidden md:block'></div>
+                </div>
+            
+            </div>
+
+            <style>
+                {
+                    `.clip-path-slant {
+                        clip-path: polygon(20% 0%, 100% 0%, 100% 100%, 0% 100%)
+                    }`
+                }
+            </style>
+        </section>
+    )
 }
 
 export default NewsletterSection;
