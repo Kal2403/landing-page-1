abe7ebc58ddcf565a028d37693df1f2f9d8b6e89
Cristhian Castillo <kal2403199255@gmail.com>
2025-04-06 22:36:21 +0200

    Add right content email input - button and icon to button

--- src/components/NewsletterSection.jsx
+++ src/components/NewsletterSection.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { HiArrowRight } from 'react-icons/hi';
 
 const NewsletterSection = () => {
     return (
@@ -14,6 +15,15 @@ const NewsletterSection = () => {
                         <div className='text-white max-w-lg text-center md:text-left'>
                             <h2 className='text-2xl sm:text-3xl lg:text-4xl font-medium mb-4'>Subscribe newsletter</h2>
                             <p className='text-blue-100 text-sm sm:text-base'>Best cooks and best delivery guys all at your service. Hot tasty food</p>
+                        </div>
+
+                        <div className='flex flex-col sm:flex-row gap-4 sm:gap-0'>
+                            <input type="email" placeholder='Enter your email address'
+                            className='w-full bg-white sm:w-auto md:w-80 px-4 sm:px-6 py-3 sm:py-4 rounded-xl sm:rounded-l-xl sm:rounded-r-none focus:outline-none' />
+                            <button className='w-full sm:w-auto cursor-pointer bg-green-500 text-white px-6 sm:px-8 py-3 sm:py-4 rounded-xl sm:rounded-l-none sm:rounded-r-lg flex items-center gap-2'>
+                                <span>Discover</span>
+                                <HiArrowRight className='size-5' />
+                            </button>
                         </div>
 
                     </div>
