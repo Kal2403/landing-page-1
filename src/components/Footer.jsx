09be8ec37e06e0ad952383137de930bfd4902b70
Cristhian Castillo <kal2403199255@gmail.com>
2025-04-06 22:47:31 +0200

    Add footer links data

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,30 @@
 import React from 'react';
 import { FaFacebookF, FaLinkedin, FaTwitter } from 'react-icons/fa';
 
+const footerLinks = {
+    company: [
+        { name: 'About', href: '#' },
+        { name: 'Terms of Use', href: '#' },
+        { name: 'Privacy Policy', href: '#' },
+        { name: 'How it Works', href: '#' },
+        { name: 'Contact Us', href: '#' },
+    ],
+    getHelp: [
+        { name: 'Support Carrer', href: '#' },
+        { name: '24h Service', href: '#' },
+        { name: 'Quick Chat', href: '#' },
+    ],
+    support: [
+        { name: 'FAQ', href: '#' },
+        { name: 'Policy', href: '#' },
+        { name: 'Business', href: '#' },
+    ],
+    contact: [
+        { name: 'WhatsApp', href: '#' },
+        { name: 'Support 24', href: '#' },
+    ],
+}
+
 const Footer = () => {
     return (
         <footer className='bg-gray-50'>
