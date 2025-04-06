3e1b044ed9f3e5b181362880a1a5ad866ef91b8c
Cristhian Castillo <kal2403199255@gmail.com>
2025-04-06 22:34:44 +0200

    Add flex layout for content and left content

--- src/components/NewsletterSection.jsx
+++ src/components/NewsletterSection.jsx
@@ -8,6 +8,15 @@ const NewsletterSection = () => {
 
                 <div className='relative md:px-16 py-16 px-6 md:py-24'>
                     <div className='absolute top-0 right-0 w-1/2 h-full bg-blue-700 clip-path-slant hidden md:block'></div>
+
+                    <div className='relative flex flex-col lg:flex-row items-center justify-between gap-8 lg:gap-12'>
+
+                        <div className='text-white max-w-lg text-center md:text-left'>
+                            <h2 className='text-2xl sm:text-3xl lg:text-4xl font-medium mb-4'>Subscribe newsletter</h2>
+                            <p className='text-blue-100 text-sm sm:text-base'>Best cooks and best delivery guys all at your service. Hot tasty food</p>
+                        </div>
+
+                    </div>
                 </div>
             
             </div>
