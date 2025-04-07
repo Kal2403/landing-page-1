befa74ad9390dabe3232671c6edb29d3bf3b424f
Cristhian Castillo <kal2403199255@gmail.com>
2025-04-07 22:47:14 +0200

    Add id to section for link navigation bar

--- src/components/ServicesSection.jsx
+++ src/components/ServicesSection.jsx
@@ -34,7 +34,7 @@ const ServicesSection = () => {
     ]
 
     return (
-        <section className='py-20 container mx-auto px-4 sm:px-6 lg:px-8'>
+        <section className='py-20 container mx-auto px-4 sm:px-6 lg:px-8' id='services'>
 
             <div className='flex flex-col md:flex-row items-center justify-between gap-12 lg:gap-24'>
 
